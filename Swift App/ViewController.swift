5202bb30d032218daa76929c32e47f1370977df2
mjanes <github@markjanes.me.uk>
2017-04-07 22:33:32 +0100

    My first commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Mark Janes on 07/04/2017.
+//  Copyright © 2017 The Light Machine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var noOfButtonTaps = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        coolLabel.text = "Nuts"
+
+        noOfButtonTaps = noOfButtonTaps + 1
+        if noOfButtonTaps > 10 {
+            coolLabel.text = "ARGGGHHH! Stop pressing the button!!"
+        }
+        
+        print(noOfButtonTaps, "Button tapped")
+    }
+
+    @IBAction func button2Tapped(_ sender: Any) {
+        coolLabel.text = "Button 2"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
